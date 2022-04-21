9dd16c6f7bf15cf018a2230d77f18eb8c4ba7f9d
konradtbudny <konradtbudny@gmail.com>
2022-04-21 18:43:59 -0500

    started front-end

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import {Api}
+const Home = () => {
+
+    return(
+        <div className="app-container">
+      <h1>Hello, World!</h1>
+      <p>API Status: {APIHealth}</p>
+    
+    </div>)
+};
+export default Home;
