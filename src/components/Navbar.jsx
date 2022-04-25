a04095d33ad5e83367dba1a784db3bd48299b867
Jaden <jadenshort231@gmail.com>
2022-04-25 10:02:36 -0500

    add buttons for the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const Navbar = () => {
+const Navbar = ({isLoggedIn}) => {
   return (
     <div>
       <div>
         <h1>Chameleonaires</h1>
       </div>
       <li>
-        <Link to="/home">Home</Link>
+        <Link to="/home"></Link>
+        <button>Home</button>
       </li>
       <li>
-        <Link to="/login">Login</Link>
+        <Link to="/login"></Link>
+        <button>Login</button>
       </li>
       <li>
-        <Link to="/register">Register</Link>
+        <Link to="/register"></Link>
+        <button>Register</button>
       </li>
       <li>
-        <Link to="/products">Products</Link>
+        <Link to="/products"></Link>
+        <button>Products</button>
       </li>
+      {isLoggedIn ? (
+          <Link to="/orders">
+            <button>My Orders</button>
+          </Link>
+        ) : null}
     </div>
   );
 };
