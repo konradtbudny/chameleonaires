46a60adbe9d2e74646301c1e0cab874d0ba4cd71
Jaden <jadenshort231@gmail.com>
2022-04-27 08:43:54 -0500

    ooohhoo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,19 @@ import { Link } from "react-router-dom";
 const Navbar = ({isLoggedIn}) => {
   return (
     <div>
-      <div>
-        <h1>Chameleonaires</h1>
-      </div>
-      <li>
-        <Link to="/home"></Link>
+      
+       <Link to="/home"></Link>
         <button>Home</button>
-      </li>
-      <li>
+     
         <Link to="/login"></Link>
         <button>Login</button>
-      </li>
-      <li>
+      
         <Link to="/register"></Link>
         <button>Register</button>
-      </li>
-      <li>
+      
         <Link to="/products"></Link>
         <button>Products</button>
-      </li>
+      
       {isLoggedIn ? (
           <Link to="/orders">
             <button>My Orders</button>
